aca6aced37b83994c47f9edec64af9c4d7ee57ba
andrewterebey <andrew.terebey@gmail.com>
2025-04-07 13:39:15 -0700

    first commit

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+    public static void main(String[] args) {
+        for (int num = 0; num <= 10; num++) {
+            System.out.println("Hello, World!");
+            System.out.println("Hello again, World");
+        }
+    }
+}
