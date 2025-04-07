8286315626544b54ec2f7caf940f0de0333f1918
andrewterebey <andrew.terebey@gmail.com>
2025-04-07 14:14:52 -0700

    Commit 2

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         for (int num = 0; num <= 10; num++) {
-            System.out.println("Hello, World!");
+            System.out.println("Test 2");
             System.out.println("Hello again, World");
         }
     }
