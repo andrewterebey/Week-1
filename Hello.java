94f86475c872f0d0f18f65612c4cf3936dd6e566
Suphachot <s.ratchaworapong@gmail.com>
2025-04-07 14:01:57 -0700

    seattle1111

--- Hello.java
+++ Hello.java
@@ -2,6 +2,8 @@ public class Hello {
     public static void main(String[] args) {
         for (int num = 0; num <= 10; num++) {
             System.out.println("test again pls");
+            System.out.println("gege");
+            System.out.println("Seattle11111");
     
         }
     }
